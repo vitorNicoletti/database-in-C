7dc22925557947cb3d3eabb137a0005036ed1312
vitorNicoletti <nicoletttivitor@gmail.com>
2023-11-07 16:20:44 -0300

    new general functions created & bin. convertion

--- gerador.c
+++ gerador.c
@@ -1,55 +1,45 @@
-
-
-/*
-
-int main()
-{
-    char palavra[MAX];
-    for(int i =0; i<MAX;i++)
-    {
-        palavra[i] = i+'a';
-    }
-    FILE* arq = fopen("teste.bin","wb");
-    fwrite(palavra,1,MAX,arq);
-    fclose(arq);
-}
-
-    typedef struct
-    {
-        char titulo[50];
-        char autor[50]
-    }Livro;
-    
-int main(){
-    Livro L;
-    strcpy(L.titulo,"ensaio");
-    strcpy(L.autor,"LuanGC");
-    FILE* arq = fopen("struct.bin","wb");
-    fwrite(&L,sizeof(Livro),1,arq);
-    fclose(arq);
-    
-}
-*/
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 typedef struct Livro Livro;
+
 struct Livro
 {
-int id;
-char titulo[50];
-Livro* proximo;
+    int id;
+    char titulo[50];
+    Livro* proximo;
 };
-Livro* lista = NULL;
-void inserir_ordenado(int novo_valor, char novo_titulo[50])
+
+void verif_arq(FILE*arq){
+    if (arq==NULL)
+    {
+        puts("erro ao abrir arquivo");
+        exit(1);
+    }
+    
+}
+void inserir_binario(Livro* lista,FILE*arq)
+{
+    verif_arq(arq);
+
+    Livro* q = lista;
+    while (q!= NULL)
+    {
+        fwrite(q,sizeof(Livro),1,arq);
+        q = q->proximo;
+    }
+}
+
+
+Livro* inserir_ordenado(int novo_valor, char novo_titulo[50],Livro* lista_daora)
 {
     Livro* novo_livro = (Livro*) malloc(sizeof(Livro));
     novo_livro->id = novo_valor;
     strcpy(novo_livro->titulo, novo_titulo);
     
     Livro* p=NULL;
-    Livro* q = lista;
+    Livro* q = lista_daora;
     bool localizado = true;
     while (q && localizado)
     {
@@ -65,40 +55,48 @@ void inserir_ordenado(int novo_valor, char novo_titulo[50])
     }
     if (p==NULL)
     {
-        novo_livro->proximo = lista;
-        lista=novo_livro;
+        novo_livro->proximo = lista_daora;
+        //lista=novo_livro;
+        
+        lista_daora = novo_livro;
     }
     else
     {
         p->proximo = novo_livro;
-        novo_livro->proximo = q;
+        novo_livro->proximo = q;  
     }
+    return lista_daora;
+    
 }
-void imprimir_lista()
+void imprimir_lista(Livro* lista)
 {
-Livro* p = lista;
-while (p)
-{
-printf("%d-%s ", p->id,p->titulo);
-p = p->proximo;
+    
+    Livro *P = lista;
+    
+    while (P != NULL)
+    {
+    printf("%d-%s ", P->id,P->titulo);
+    P = P->proximo;
+    }
+    putchar('\n');
 }
-putchar('\n');
-}
+
 int main()
 {
-    FILE* arq = fopen("texto-base.txt","r");
-    if (arq == NULL)
-    {
-        puts("falha ao tentar abrir o arquivo");
-        exit(1);
-    }
+    Livro* lista = NULL;
+    FILE* arq = fopen("D:/Jogos/code/Programacao Imperativa/trabalho 2/texto-base.txt","r");
+    verif_arq(arq);
     int id;
     char titulo[50];
     while (fscanf(arq,"%d\n%s",&id,titulo) != EOF)
     {
-        inserir_ordenado(id,titulo);
+        lista = inserir_ordenado(id,titulo,lista);
+        
     }
     fclose(arq);
-    
-    imprimir_lista();
+    printf("%d",lista->id);
+    puts("\n");
+    imprimir_lista(lista);
+    arq = fopen("D:/Jogos/code/Programacao Imperativa/trabalho 2/texto-base.bin","wb");
+    inserir_binario(lista,arq); 
 }
